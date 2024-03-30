dffed05545a56cfe439651f4361e01f93fb1e6c0
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-03-30 19:16:44 -0400

    added scouting page

--- app/src/main/java/com/fro/businesspitscouting/MainActivity.java
+++ app/src/main/java/com/fro/businesspitscouting/MainActivity.java
@@ -2,13 +2,30 @@ package com.fro.businesspitscouting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //declaring
+    //buttons
+    TextView startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //instantiating
+        //button
+        startButton = findViewById(R.id.startButton);
+
+        // changes to next page on click
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ScoutingActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fro/businesspitscouting/ScoutingActivity.java
@@ -0,0 +1,14 @@
+package com.fro.businesspitscouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScoutingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scouting);
+    }
+}
